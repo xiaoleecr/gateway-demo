a79e096270071518d19cf91b06484c20bd6bb0c6
licongrui <xiaoleecr@live.cn>
2022-03-11 11:05:45 +0800

    redis使用

--- hello-application/src/test/java/com/lcr/helloapplication/HelloApplicationTests.java
+++ hello-application/src/test/java/com/lcr/helloapplication/HelloApplicationTests.java
@@ -1,13 +1,31 @@
 package com.lcr.helloapplication;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class HelloApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("hello");
     }
 
+    public static void main(String[] args) {
+        System.out.println(m1());
+    }
+
+    public static String m1() {
+        try {
+            log.info("return");
+            return "return";
+        } catch (Exception e) {
+            log.info("catch");
+            return "catch";
+        } finally {
+            log.info("finally");
+        }
+    }
 }
